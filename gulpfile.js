e4c01a07d0996336ce23b98c606c8fe5cd913d6c
Cody Casey <codyjamescasey@gmail.com>
2015-03-18 21:17:58 -0400

    converted project to not used React, changed up gulp file, added bios folder to contain text for bios until they are in the page

--- gulpfile.js
+++ gulpfile.js
@@ -1,148 +1,121 @@
 var gulp = require('gulp'),
-    // Generic imports
-    Stream = require('stream'),
-    gutil = require('gulp-util'),
-    path = require('path'),
-    clean = require('rimraf'),
-    plumber = require('gulp-plumber'),
-    // Browserify-related imports
-    browserify = require('browserify'),
-    source = require('vinyl-source-stream'),
-    watchify = require('watchify'),
-    reactify = require('reactify'),
-    // LESS-related imports
     less = require('gulp-less'),
-    sourcemaps = require('gulp-sourcemaps'),
-    // HTML-related imports
-    minify = require('gulp-minify-html'),
-    // Dev-server-related imports
-    nodemon = require('nodemon');
+    path = require('path'),
+    concat = require('gulp-concat'),
+    replace = require('gulp-replace'),
+    nodemon = require('gulp-nodemon'),
+    clean = require('rimraf'),
+    livereload = require('gulp-livereload'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    crypto = require('crypto'),
+    async = require('async'),
+    exec = require('child_process').exec,
+    http = require('http'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    htmlmin = require('gulp-htmlmin');
 
-var helpers = {
-    rebundle: function(bundler, done) {
-        var time = (new Date()).getTime();
-        gutil.log('Re-bundling js started');
-        bundler
-            .bundle(function(err) {
-                if (!err) {
-                    gutil.log('Re-bundling js finished after ' + (((new Date()).getTime() - time) / 1000) + ' seconds');
-                    if (done) done();
-                } else {
-                    gutil.log('Re-bundling js FAILED after ' + (((new Date()).getTime() - time) / 1000) + ' seconds');
-                    console.log(err);
-                    if (done) done();
-                }
-            })
-            .pipe(plumber())
-            .pipe(source(path.join(__dirname, 'main.js')))
-            .pipe(gulp.dest(path.join(__dirname, 'dist', 'js')))
-    },
-    delay: function(callback) {
-        // Waits a second before executing a function
-        return function() {
-            setTimeout(function() {
-            }, 1000);
-        };
-    }
+var delay = function(fn, time) {
+    return function() {
+        setTimeout(fn, time);
+    };
 };
 
-// Compiles the public js
-gulp.task('browserify', function() {
-    var bundler = browserify({
-        cache: {},
-        packageCache: {},
-        fullPaths: true
-    });
-    // React middleware for JSX
-    bundler.transform(reactify);
-    // Add the entry point
-    bundler.add(path.join(__dirname, 'client', 'js', 'main.js'));
-    // Perform initial rebundle
-    return helpers.rebundle(bundler);
-});
-
-// Watches and recompiles public js
-gulp.task('watchify', function() {
-    var bundler = browserify({
-        cache: {},
-        packageCache: {},
-        fullPaths: true,
-        debug: true
-    });
-    // Pass the browserify bundler to watchify
-    bundler = watchify(bundler);
-    // React middleware for JSX
-    bundler.transform(reactify);
-    // Bundlize on updates
-    bundler.on('update', function() {
-        helpers.rebundle(bundler);
-    });
-    // Add the entry point
-    bundler.add(path.join(__dirname, 'client', 'js', 'main.js'));
-    // Perform initial rebundle
-    return helpers.rebundle(bundler);
-});
-
-// Compiles the public less
-gulp.task('less', function() {
-    gulp.src(path.join('client', 'less', 'main.less'))
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
+gulp.task('less-dev', function() {
+    // Builds the CSS
+    gulp.src(['client/less/main.less'])
         .pipe(less())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.join('.', 'dist', 'css')));
+        .pipe(gulp.dest('dist'));
 });
 
-// Condenses the pages
-gulp.task('pages', function() {
-    gulp.src('./client/pages/*.html')
-        .pipe(plumber())
-        .pipe(minify({
-            empty: true,
-            spare: true
+gulp.task('less-prod', function() {
+    // Builds the CSS
+    gulp.src(['client/less/main.less'])
+        .pipe(less())
+        .pipe(minifyCSS({
+            advanced: true
         }))
-        .pipe(gulp.dest(path.join('.', 'dist', 'pages')));
+        .pipe(gulp.dest('dist'));
 });
 
-// Condenses the images
-gulp.task('images', function() {
-    // TODO: image compression
-    gulp.src('./client/img/**/*')
-        .pipe(plumber())
-        .pipe(gulp.dest(path.join('.', 'dist', 'img')));
+gulp.task('js-dev', function() {
+    // Builds the JS
+    gulp.src(['client/js/main.js'])
+        .pipe(gulp.dest('dist'));
+    gulp.src(['client/js/conduct.js'])
+        .pipe(gulp.dest('dist'));
 });
 
-// Condenses the images
-gulp.task('images-delayed', function() {
-    // TODO: image compression
-    setTimeout(function() {
-        gulp.src('./client/img/**/*')
-            .pipe(plumber())
-            .pipe(gulp.dest(path.join('.', 'dist', 'img')))
-            .on('error', helpers.smother);
-    }, 500);
+gulp.task('js-prod', function() {
+    // Builds the JS
+    gulp.src(['client/js/main.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+    gulp.src(['client/js/conduct.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
 
-// Clears all compiled public code
+gulp.task('html-dev', function() {
+    // Moves and Compresses HTML
+    gulp.src(['client/pages/*.html'])
+        .pipe(replace('</body>', '<script src="http://localhost:35729/livereload.js"></script></body>'))
+        .pipe(gulp.dest('dist/pages'));
+});
+
+gulp.task('html-prod', function() {
+    // Moves and Compresses HTML
+    gulp.src(['client/pages/*.html'])
+        .pipe(htmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            caseSensitive: true,
+            minifyJS: true,
+            keepClosingSlash: true
+        }))
+        .pipe(gulp.dest('dist/pages'));
+});
+
+gulp.task('assets', function() {
+    gulp.src(['client/img/**/*'])
+        .pipe(gulp.dest('dist/img'));
+    gulp.src(['client/vendor/**/*'])
+        .pipe(gulp.dest('dist/vendor'));
+    gulp.src(['client/files/**/*'])
+        .pipe(gulp.dest('dist/files'));
+});
+
+gulp.task('server-dev', function() {
+    // Runs the server forever
+    nodemon({
+        script: 'index.js',
+        ext: 'js',
+        ignore: [
+            'client/*',
+            'dist'
+        ],
+        env: {
+            PORT: '80'
+        }
+    });
+});
+
+gulp.task('watch', ['server-dev'], function() {
+    livereload.listen();
+    gulp.watch('client/js/*.js', ['js-dev']).on('change', livereload.changed);
+    gulp.watch('client/less/*.less', ['less-dev']).on('change', delay(livereload.changed, 500));
+    gulp.watch('client/pages/*.html', ['html-dev']).on('change', livereload.changed);
+});
+
+// Clears all compiled client code
 gulp.task('clean', function() {
     clean.sync(path.join(__dirname, 'dist'));
 });
 
-// Watches changes to the public code
-gulp.task('watch', ['clean', 'less', 'pages', 'images', 'watchify'], function() {
-    gulp.watch('client/pages/*.html', ['pages']);
-    gulp.watch('client/less/**/*.less', ['less']);
-    gulp.watch('client/img/**/*', ['images-delayed']);
-});
-
-// Runs dev server and watches public code
-gulp.task('dev', ['watch'], function() {
-    nodemon({
-        script: 'index.js',
-        ext: 'js',
-        ignore: ['client/**/*']
-    });
-});
-
-// Run all compilation tasks
-gulp.task('default', ['clean', 'less', 'pages', 'images', 'browserify']);
+gulp.task('build', ['html-dev', 'js-dev', 'less-dev', 'assets']);
+gulp.task('deploy', ['html-prod', 'js-prod', 'less-prod', 'assets']);
+gulp.task('dev', ['clean', 'build', 'watch']);
+gulp.task('prod', ['clean', 'deploy']);
+gulp.task('default', ['dev']);
